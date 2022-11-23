593d0bf96ba832ce80f80c5f986b7d306785b7dd
Quentin Gendreaud <quentin.gendreaud@gmail.com>
2022-11-23 22:01:05 +0100

    update user.service

---
+++ src/app/enum/session-key.enum.ts
@@ -0,0 +1,3 @@
+export enum SessionKeyEnum {
+    User = 'user'
+}
--- src/shared/services/user.service.ts
+++ src/shared/services/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { User } from 'src/app/interfaces/user.model';
+import { Observable } from 'rxjs';
+import { SessionKeyEnum } from 'src/app/enum/session-key.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +12,15 @@ export class UserService {
 
   constructor(private readonly http: HttpClient) { }
 
-  login(login: string, password: string) {
-    return this.http.post(`${environment.apiUrl}login`, {
+  login(login: string, password: string): Observable<User> {
+    return this.http.post<User>(`${environment.apiUrl}login`, {
       login,
       password
     });
+  }
+
+  getUser(): User | undefined {
+    const sessionUser = window.sessionStorage.getItem(SessionKeyEnum.User);
+    return sessionUser ? JSON.parse(sessionUser) : undefined;
   }
 }
